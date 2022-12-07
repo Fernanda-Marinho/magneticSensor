54e265d79096370377ad302d2b9ce8ca72dd11f9
Fernanda-Marinho <fmarinho1918@gmail.com>
2022-12-07 01:13:01 -0300

    Primeira medição

---
+++ codigoSensor/codigoSensor.ino
@@ -0,0 +1,22 @@
+#define SENSOR 5
+int resp = 0;
+int cont = 0;
+
+
+void setup() {
+  pinMode(SENSOR, INPUT);
+  
+}
+
+void loop() {
+  resp = digitalRead(SENSOR);
+  if (resp == HIGH){
+    digitalWrite(LED_BUILTIN, HIGH); 
+    cont++; 
+    Serial.println(cont);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, LOW); 
+  }
+
+}
