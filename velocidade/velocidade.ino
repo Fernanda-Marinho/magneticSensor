3b6078e16b9823f77e89a6f024559bc75f71035f
Fernanda-Marinho <fmarinho1918@gmail.com>
2023-09-01 17:53:30 -0300

    velocidade linear + angular

--- velocidade/velocidade.ino
+++ velocidade/velocidade.ino
@@ -1,28 +1,33 @@
 #define SENSOR_R 2
 #define SENSOR_L 3
+#define DIST_ENTRE_RODAS 25.5
 
 volatile unsigned long tempoAnteriorR = 0;
 volatile unsigned long tempoAnteriorL = 0;
 const float distanciaPorPulso = 0.008125; 
+float velocidadeR;
+float velocidadeL;
+float linear = 0.0;
+float angular = 0.0;
 
 void contaPulsoR() {
   unsigned long tempoAtual = micros(); 
   unsigned long tempoDecorrido = tempoAtual - tempoAnteriorR;
   tempoAnteriorR = tempoAtual;
-  float velocidadeR = distanciaPorPulso / (tempoDecorrido / 1000000.0); 
-  Serial.print("Velocidade direita: ");
-  Serial.print(velocidadeR);
-  Serial.println(" m/s");
+  velocidadeR = distanciaPorPulso / (tempoDecorrido / 1000000.0); 
+  //Serial.print("Velocidade direita: ");
+  //Serial.print(velocidadeR);
+  //Serial.println(" m/s");
 }
 
 void contaPulsoL() {
   unsigned long tempoAtual = micros(); 
   unsigned long tempoDecorrido = tempoAtual - tempoAnteriorL;
   tempoAnteriorL = tempoAtual;
-  float velocidadeL = distanciaPorPulso / (tempoDecorrido / 1000000.0); 
-  Serial.print("Velocidade esquerda: ");
-  Serial.print(velocidadeL);
-  Serial.println(" m/s");
+  velocidadeL = distanciaPorPulso / (tempoDecorrido / 1000000.0); 
+  //Serial.print("Velocidade esquerda: ");
+  //Serial.print(velocidadeL);
+  //Serial.println(" m/s");
 }
 
 void setup() {
@@ -34,5 +39,13 @@ void setup() {
 }
 
 void loop() {
+  linear = (velocidadeR + velocidadeL) / 2; 
+  angular = (velocidadeR - velocidadeL) / DIST_ENTRE_RODAS;
+  Serial.begin(9600);
+  Serial.print("Linear: ");
+  Serial.println(linear); 
+  Serial.print("Angular: ");
+  Serial.println(angular);
+  delay(100);
   
 }
